46139804865e51cf1cc7ed75b544c357f0281194
= <=>
2018-01-22 20:52:06 +0000

    ...

---
+++ register.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Create an account</title>
+</head>
+<body>
+    <div id="inputContainer">
+        <form id="loginForm" action="register.php" method="post">
+            <h2>Login to your account</h2>
+            <input id="loginUsername" name=loginUsername" type="text">
+        </form>
+    </div>
+</body>
+</html>
