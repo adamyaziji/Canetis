da3bde7723d31fe5b7974421112259e3d0f14d2b
= <=>
2018-01-22 22:10:35 +0000

    Login/Register page and sanitisation

---
+++ includes/handlers/register-handler.php
@@ -0,0 +1,30 @@
+<?php
+
+function sanitizeFormUsername($inputText){
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ","", $inputText);
+    return $inputText;
+}
+
+function sanitizeFormString($inputText){
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ","", $inputText);
+    $inputText = ucfirst(strtolower($inputText));
+    return $inputText;
+}
+
+function sanitizeFormPassword($inputText){
+    $inputText = strip_tags($inputText);
+    return $inputText;
+}
+
+
+if(isset($_POST['registerButton'])) {
+    $username = sanitizeFormUsername($_POST['userName']);
+    $firstname = sanitizeFormString($_POST['firstName']);
+    $lastname = sanitizeFormString($_POST['lastName']);
+    $email = sanitizeFormString($_POST['email']);
+    $email2 = sanitizeFormString($_POST['email2']);
+    $password = sanitizeFormPassword($_POST['password']);
+    $password = sanitizeFormPassword($_POST['password2']);
+}
--- register.php
+++ register.php
@@ -1,12 +1,64 @@
+<?php
+
+include "includes/handlers/register-handler.php";
+include "includes/handlers/login-handler.php";
+
+
+?>
+
+
 <html>
 <head>
-    <title>Create an account</title>
+    <title>Log in</title>
 </head>
 <body>
     <div id="inputContainer">
         <form id="loginForm" action="register.php" method="post">
             <h2>Login to your account</h2>
-            <input id="loginUsername" name=loginUsername" type="text">
+            <p>
+                <label for="loginUsername">Username: </label>
+                <input id="loginUsername" name=loginUsername" placeholder="Username" required type="text">
+            </p>
+            <p>
+                <label for="loginPassword">Password: </label>
+                <input id="loginPassword" name=loginPassword" placeholder="Password" required type="password">
+            </p>
+            <button type="submit" name="loginButton">Login</button>
+        </form>
+
+
+        <form id="registerForm" action="register.php" method="post">
+            <h2>Create an account</h2>
+            <p>
+                <label for="userName">Username: </label>
+                <input id="userName" name="userName" placeholder="Username" required type="text">
+            </p>
+            <p>
+                <label for="firstName">First Name: </label>
+                <input id="firstName" name="firstName" placeholder="e.g. John" required type="text">
+            </p>
+            <p>
+                <label for="lastName">Last Name: </label>
+                <input id="lastName" name="lastName" placeholder="e.g. Johnson" required type="text">
+            </p>
+            <p>
+                <label for="email">Email: </label>
+                <input id="email" name="email" placeholder="e.g. john@gmail.com" required type="text">
+            </p>
+
+            <p>
+                <label for="email">Confirm email: </label>
+                <input id="email2" name="email2" required type="text">
+            </p>
+            <p>
+                <label for="password">Password: </label>
+                <input id="password" name="password" placeholder="Password" required type="password">
+            </p>
+            <p>
+                <label for="password2">Confirm Password: </label>
+                <input id="password2" name="password2" placeholder="Password" required type="password">
+            </p>
+            <button type="submit" name="registerButton">Sign Up!</button>
         </form>
     </div>
 </body>
